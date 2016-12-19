b031fd463a99703ffbd098b62588c9a1bdae91d2
Takehiro KAMADA <tadamak@timedia.co.jp>
2016-12-19 13:49:27 +0900

    add dynamodb_store settings

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_dynamodb_session_store_sample_session'
+AWS.config({ region: "ap-northeast-1" })
+Rails.application.config.session_store :dynamodb_store
